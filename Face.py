4c0e477c01d4a9a0bbf30caf6b8b64e7ea0b352a
MunKwangsu <kwangsu.mun@gmail.com>
2024-01-20 22:00:17 +0900

    first commit

---
+++ Face.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from keras.utils import load_img, img_to_array
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
+
+classes = ["20歳以下","20代","30代","40代","50代","60代","70代","80代","90代"]
+image_size = 224
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./MobileNet.h5', compile=False)#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = img_to_array(load_img(filepath))
+            img = cv2.resize(img, (224, 224))
+            data = np.expand_dims(img, axis = 0)
+            data = data.astype(np.float32) / 255.0
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(data)[0]
+            predicted = result.argmax()
+            pred_answer = "あなたは " + classes[predicted] + " です"
+
+            return render_template("index.html",answer=pred_answer)
+
+    return render_template("index.html",answer="")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
